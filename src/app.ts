c66319b0d92464cefba4c570170e0e29cdb1a0b9
Fede <federico.pochat@ing.austral.edu.ar>
2022-03-23 12:34:10 -0300

    Cors added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-
+const cors = require('cors');
 import express,{Request,Response, Application} from 'express';
 import {connect} from "./infrastracture/db/database";
 import {router} from "./infrastracture/router/route";
 
 const app: Application= express();
-
+app.use(cors())
 app.use(router);
 
 const startServer = (): void => {
