d00027525ba35af6970e9e71a3874c34ad1aebaa
Fede <federico.pochat@ing.austral.edu.ar>
2022-06-14 12:07:16 -0300

    Fixed GetPlayer

---
+++ src/infrastracture/router/player/getplayer.route.ts
@@ -0,0 +1,11 @@
+import {GetCoursesControllerProvider} from "../../providers/course/getCourseController.provider";
+import {ABMPlayerControllerProvider} from "../../providers/player/abmPlayerController.provider";
+import {Player} from "../../../domain/models/player.model";
+import {verifyTokenPlayer} from "./verifyTokenPlayer";
+
+export const getPlayerInfo = async ({id}: any, req: Request): Promise<Player> => {
+    // @ts-ignore
+    //const token: string = <string>req.headers['authorization'];
+    //const username: string = verifyTokenPlayer(token.substring(7));
+    return await ABMPlayerControllerProvider.getController().getPlayer(id);
+};
